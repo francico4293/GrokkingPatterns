382fbaa73e3f4f828f9f28ff46247910842dd5ed
Colin Francis <francico@oregonstate.edu>
2023-02-14 14:17:20 -0500

    linked list cycle

---
+++ src/main/java/helpers/ListNode.java
@@ -0,0 +1,11 @@
+package helpers;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
